ca0effd84bdb2ec1cef2c36f7fdf37221889d9af
Jonathan Karr <jonrkarr@gmail.com>
2021-01-05 03:59:53 -0500

    updating to biosimulators-util 0.1.30; incrementing version

--- biosimulators_amici/core.py
+++ biosimulators_amici/core.py
@@ -13,8 +13,10 @@ from biosimulators_utils.report.data_model import ReportFormat, DataGeneratorVar
 from biosimulators_utils.sedml.data_model import (Task, ModelLanguage, UniformTimeCourseSimulation,  # noqa: F401
                                                   DataGeneratorVariable, DataGeneratorVariableSymbol)
 from biosimulators_utils.sedml import validation
+from biosimulators_utils.sedml.exec import exec_sed_doc
 from biosimulators_utils.utils.core import validate_str_value, parse_value
 import amici
+import functools
 import importlib.util
 import numpy
 import os.path
@@ -54,7 +56,8 @@ def exec_sedml_docs_in_combine_archive(archive_filename, out_dir,
         bundle_outputs (:obj:`bool`, optional): if :obj:`True`, bundle outputs into archives for reports and plots
         keep_individual_outputs (:obj:`bool`, optional): if :obj:`True`, keep individual output files
     """
-    exec_sedml_docs_in_archive(archive_filename, exec_sed_task, out_dir,
+    sed_doc_executer = functools.partial(exec_sed_doc, exec_sed_task)
+    exec_sedml_docs_in_archive(sed_doc_executer, archive_filename, out_dir,
                                apply_xml_model_changes=True,
                                report_formats=report_formats,
                                plot_formats=plot_formats,
